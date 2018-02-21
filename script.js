ffe0caad25b187f0fa09baa0869ce01e0c307ee1
Eduardo <36666596+Eduardo110298@users.noreply.github.com>
2018-02-21 13:37:12 -0400

    Little change to script

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-//Removing checkout form.
-var checkoutNode = document.getElementsByClassName("woocommerce")[1].children[2]
-document.getElementsByClassName("woocommerce")[1].removeChild(checkoutNode)
+if (location.pathname == "/checkout/"){
+  //Removing checkout form.
+  var checkoutNode = document.getElementsByClassName("woocommerce")[1].children[2]
+  document.getElementsByClassName("woocommerce")[1].removeChild(checkoutNode)
 
-//Making up the container with info.
-var checkoutInfo = document.createElement('DIV')
-checkoutInfo.className = "woocommerce-info"
+  //Making up the container with info.
+  var checkoutInfo = document.createElement('DIV')
+  checkoutInfo.className = "woocommerce-info"
 
-//The toggle switch
-var checkoutInfoLInk = document.createElement('A')
-checkoutInfoLink.href = "#"
-checkoutInfoLink.id = "checkoutInfoLink"
-checkoutInfoLink.innerText = "Or you can click here to continue and register"
+  //The toggle switch
+  var checkoutInfoLInk = document.createElement('A')
+  checkoutInfoLink.href = "#"
+  checkoutInfoLink.id = "checkoutInfoLink"
+  checkoutInfoLink.innerText = "Or you can click here to continue and register"
 
-//Putting the switch into container.
-checkoutInfo.appendChild(checkoutInfoLink)
+  //Putting the switch into container.
+  checkoutInfo.appendChild(checkoutInfoLink)
 
-//Putting the new container into the woocommerce page.
-document.getElementsByClassName("woocommerce")[1].appendChild(checkoutInfo)
+  //Putting the new container into the woocommerce page.
+  document.getElementsByClassName("woocommerce")[1].appendChild(checkoutInfo)
 
-//Preparing the checkout form.
-checkoutNode.id = "checkoutNode"
-checkoutNode.style.display = "none"
+  //Preparing the checkout form.
+  checkoutNode.id = "checkoutNode"
+  checkoutNode.style.display = "none"
 
-//Putting it into the woocommerce page.
-document.getElementsByClassName("woocommerce")[1].appendChild(checkoutNode)
+  //Putting it into the woocommerce page.
+  document.getElementsByClassName("woocommerce")[1].appendChild(checkoutNode)
 
-//The slideToggle logic:
-jQuery("#checkoutInfoLink").click(function(){
-  jQuery("#checkoutNode").slideToggle(700);
-})
+  //The slideToggle logic:
+  jQuery("#checkoutInfoLink").click(function(){
+    jQuery("#checkoutNode").slideToggle(700);
+  })
+}
