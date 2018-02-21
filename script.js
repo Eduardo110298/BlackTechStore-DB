1b8325ce9717216a188258527e858376415fb976
Eduardo <36666596+Eduardo110298@users.noreply.github.com>
2018-02-21 13:35:36 -0400

    Update to script.js for woocommerce page.

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+//Removing checkout form.
 var checkoutNode = document.getElementsByClassName("woocommerce")[1].children[2]
 document.getElementsByClassName("woocommerce")[1].removeChild(checkoutNode)
+
+//Making up the container with info.
 var checkoutInfo = document.createElement('DIV')
 checkoutInfo.className = "woocommerce-info"
-checkoutInfo.innerText = "Or you can"
+
+//The toggle switch
 var checkoutInfoLInk = document.createElement('A')
 checkoutInfoLink.href = "#"
 checkoutInfoLink.id = "checkoutInfoLink"
+checkoutInfoLink.innerText = "Or you can click here to continue and register"
+
+//Putting the switch into container.
+checkoutInfo.appendChild(checkoutInfoLink)
+
+//Putting the new container into the woocommerce page.
+document.getElementsByClassName("woocommerce")[1].appendChild(checkoutInfo)
+
+//Preparing the checkout form.
+checkoutNode.id = "checkoutNode"
+checkoutNode.style.display = "none"
+
+//Putting it into the woocommerce page.
+document.getElementsByClassName("woocommerce")[1].appendChild(checkoutNode)
+
+//The slideToggle logic:
+jQuery("#checkoutInfoLink").click(function(){
+  jQuery("#checkoutNode").slideToggle(700);
+})
