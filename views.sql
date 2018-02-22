49cfe00a1302d70c1823779dd66eeebc268c386b
Hilary Alzate <hilaryalzate@gmail.com>
2018-02-22 00:43:19 -0400

    Third View done

--- views.sql
+++ views.sql
@@ -1,22 +1,31 @@
 --Vista de emitir reporte de los equipos asignados a los tecnicos y el estatus de la orden ordenado por tecnico y fecha de recepcion
 CREATE VIEW vEquipo 
 	AS SELECT tecnico.cedula, tecnico.nombre, orden.fec_ent, pertenece_a.serial, orden.estado 
-	from tecnico tec 
+	FROM tecnico tec 
 	INNER JOIN reparacion rep 
-	  on tec.cedula = rep.cedula
+	  ON tec.cedula = rep.cedula
 	INNER JOIN orden ord
-	  on rep.nro_orden = ord.nro_orden
+	  ON rep.nro_orden = ord.nro_orden
 	INNER JOIN pertenece_a pert 
-	  on pert.nro_orden = ord.nro_orden
+	  ON pert.nro_orden = ord.nro_orden
 	ORDER BY tecnico.cedula, orden.fec_ent ASC;
 
 --Vista de los Reportes de los ingresos semanales obtenidos por tipo de reparacion 
 CREATE VIEW vIngresoTipos
 	AS equipo.tipo, SUM(factura.total)
-	from factura fac 
+	FROM factura fac 
 	INNER JOIN pertenece_a pert
-	  on fac.nro_orden = pert.nro_orden
+	  ON fac.nro_orden = pert.nro_orden
 	INNER JOIN equipo equ 
-	  on equ.serial = pert.serial
+	  ON equ.serial = pert.serial
 	WHERE factura.fecha BETWEEN sysdate - 7 AND sysdate
 	GROUP BY (equipo.tipo);
+
+--Vista de los repuestos diarios utilizados en las ordenes 
+CREATE VIEW vRepuesto 
+	AS SELECT repuesto.codigo, repuesto.nombre, repuesto.costo, orden.nro_orden, orden.estado, orden.concepto, orden.fec_ent, orden.fec_sal
+	FROM repuesto rep 
+	INNER JOIN usa u 
+	  ON u.codigo = rep.codigo
+	INNER JOIN orden ord
+	  ON ord.nro_orden = u.nro_orden;
