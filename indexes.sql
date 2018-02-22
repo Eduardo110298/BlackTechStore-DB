8f76e76f24df8d606ec27537fbf8927ce7b72326
Eduardo <eduardo_110298@hotmail.com>
2018-02-21 21:33:59 -0400

    Relationships revision (4 remain)

---
+++ indexes.sql
@@ -0,0 +1,18 @@
+/* Indices para DATABASE BlackTechStore
+
+	EN PROCESO...
+*/
+
+-- CREATE INDEX nombre_indice ON nombre_tabla (columna, columna1,….);
+
+
+CREATE INDEX repuesto_cant_existencia ON repuesto(nombre,cant_existencia); -- Indice para agilizar la busqueda de la existencia de repuestos
+
+
+CREATE INDEX equipo_estatus_equipo ON (); 
+
+
+CREATE INDEX estatus_orden ON orden(estatus);
+
+
+CREATE INDEX estatus_pago ON pago(fecha,tipo,doc_identidad_cliente);
