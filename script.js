292976c14c8619b01025febc23edff85511ea6ed
Eduardo <eduardo_110298@hotmail.com>
2018-02-23 13:16:11 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var checkoutNode = document.getElementsByClassName("woocommerce")[1].children[2]
-document.getElementsByClassName("woocommerce")[1].removeChild(checkoutNode)
-var checkoutInfo = document.createElement('DIV')
-checkoutInfo.className = "woocommerce-info"
-checkoutInfo.innerText = "Or you can"
-var checkoutInfoLInk = document.createElement('A')
-checkoutInfoLink.href = "#"
-checkoutInfoLink.id = "checkoutInfoLink"
