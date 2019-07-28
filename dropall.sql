eeb384cf389f6ab5be9bcbbcc70daa41660b8f97
Eduardo Rodríguez <eduardo110298@gmail.com>
2019-07-28 18:59:26 -0400

    Update dropall.sql

--- dropall.sql
+++ dropall.sql
@@ -2,6 +2,6 @@
 @drop-scripts/drop-triggers.sql
 @drop-scripts/drop-sequences.sql
 @drop-scripts/drop-indexes.sql
-@drop-scripts/drop-relationships.sql
+@drop-scripts/drop-tables.sql
 @drop-scripts/drop-users.sql
 @drop-scripts/drop-tablespaces.sql
